a210e914797b121eb9192dbde46fa483b88fef95
xiaochun <xchun90@163.com>
2024-05-14 23:40:22 +0800

    Fix logger error

--- zrlog-plugin-common/src/main/java/com/zrlog/plugin/RunConstants.java
+++ zrlog-plugin-common/src/main/java/com/zrlog/plugin/RunConstants.java
@@ -2,6 +2,8 @@ package com.zrlog.plugin;
 
 import com.zrlog.plugin.type.RunType;
 
+import java.util.Objects;
+
 public class RunConstants {
-    public static RunType runType = RunType.DEV;
+    public static RunType runType = Objects.equals(System.getenv("PLUGIN_RUN_TYPE"),"DEV") ? RunType.DEV : RunType.BLOG;
 }
