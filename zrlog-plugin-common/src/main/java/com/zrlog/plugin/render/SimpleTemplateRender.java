fbcf0732d2ed8deaae62c6bd193cadd9556ce448
xiaochun <xchun90@163.com>
2024-06-18 19:45:39 +0800

    Support plugin info

--- zrlog-plugin-common/src/main/java/com/zrlog/plugin/render/SimpleTemplateRender.java
+++ zrlog-plugin-common/src/main/java/com/zrlog/plugin/render/SimpleTemplateRender.java
@@ -1,9 +1,11 @@
 package com.zrlog.plugin.render;
 
+import com.google.gson.Gson;
 import com.zrlog.plugin.common.IOUtil;
 import com.zrlog.plugin.message.Plugin;
 
 import java.io.InputStream;
+import java.util.HashMap;
 import java.util.Map;
 
 public class SimpleTemplateRender implements IRenderHandler {
@@ -15,8 +17,13 @@ public class SimpleTemplateRender implements IRenderHandler {
 
     @Override
     public String render(InputStream inputStream, Plugin plugin, Map<String, Object> map) {
+        Map<String,Object> pluginMap = new Gson().fromJson(new Gson().toJson(plugin),Map.class);
+        Map<String,Object> dataMap = new HashMap<>(map);
+        pluginMap.forEach((k,v) -> {
+            dataMap.put("_plugin." + k,v);
+        });
         String renderResult = IOUtil.getStringInputStream(inputStream);
-        for (Map.Entry<String, Object> entry : map.entrySet()) {
+        for (Map.Entry<String, Object> entry : dataMap.entrySet()) {
             if (entry.getValue() != null) {
                 renderResult = renderResult.replace("${" + entry.getKey() + "}", entry.getValue().toString());
             } else {
