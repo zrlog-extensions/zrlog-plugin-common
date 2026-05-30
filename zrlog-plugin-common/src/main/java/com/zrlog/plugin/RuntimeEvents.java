428a6cd79edbeedd2e62954edaa627c237212a33
xiaochun <90xchun@gmail.com>
2026-05-30 22:05:30 +0800

    feat: v4.0 new plugin

---
+++ zrlog-plugin-common/src/main/java/com/zrlog/plugin/RuntimeEvents.java
@@ -0,0 +1,10 @@
+package com.zrlog.plugin;
+
+public class RuntimeEvents {
+
+    public static final String REFRESH_CACHE = "system.refreshCache";
+    public static final String LEGACY_REFRESH_CACHE = "refresh_cache";
+
+    private RuntimeEvents() {
+    }
+}
